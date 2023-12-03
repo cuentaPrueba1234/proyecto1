3edc11b270023b14f2dfb80a5ce068e7cc7888f8
Usu2 Name <usu2@example.com>
2023-12-03 04:45:30 +0100

    Valido cambios en Cambio3 en proyecto2

---
+++ Prueba1/src/cambio1/Cambio3.java
@@ -0,0 +1,11 @@
+package cambio1;
+
+public class Cambio3 {
+
+	public static void main(String[] args) {
+		System.out.println("Clase Cambio3 en proyecto2");
+		System.out.println("Clase Cambio3 en proyecto2");
+
+	}
+
+}
